12781fb86f7cb8b6b5101feb02a7d68fa6548ed3
Ross Foley <ross3141@gmail.com>
2015-02-11 19:35:05 -0500

    Add more comments

--- src/mqp/ebt/EBTProblem.java
+++ src/mqp/ebt/EBTProblem.java
@@ -6,21 +6,31 @@ import org.jgap.gp.impl.GPConfiguration;
 import org.jgap.gp.impl.GPGenotype;
 
 /**
- * Created by rossfoley on 2/11/15.
+ * GPProblem that handles creating a population of EBTs
+ * @author Ross Foley and Karl Kuhn
  */
 public class EBTProblem extends GPProblem {
     GPConfiguration config;
+
     public EBTProblem(GPConfiguration c) throws InvalidConfigurationException {
         super(c);
         config = c;
         config.setFitnessFunction(new MarioFitnessFunction());
     }
 
+    /**
+     * Create a population of EBTs
+     * @return the EBT genotype
+     * @throws InvalidConfigurationException
+     */
     @Override
     public GPGenotype create() throws InvalidConfigurationException {
         return null;
     }
 
+    /**
+     * Start the evolving process in a new thread
+     */
     public void start() {
         // Run the evolving thread
     }
